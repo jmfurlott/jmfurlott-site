fcd4fa1bc9b7899e02b6a676ba4d515362ff849f
Joseph Furlott <jmfurlott@gmail.com>
2014-12-22 23:14:50 -0500

    Cleaning up; small changes

--- js/header.js
+++ js/header.js
@@ -1,8 +1,10 @@
 $(function() {
-    $('.header__hamburger').click(function(event) {
+    $('.header__hamburger, .header__close').click(function(event) {
         event.stopPropagation();
         event.preventDefault();
         $('.dropdown').toggleClass('open');
+        $('.header__hamburger').toggleClass('open');
+        $('.header__close').toggleClass('open');
+        
     });
-
 });
